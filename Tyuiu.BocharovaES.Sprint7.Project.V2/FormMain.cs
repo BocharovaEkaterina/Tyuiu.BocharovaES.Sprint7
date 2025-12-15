3478858edbc2e5a5888925b2437404050e698558
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-12-15 23:33:24 +0500

    Add project files.

---
+++ Tyuiu.BocharovaES.Sprint7.Project.V2/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.BocharovaES.Sprint7.Project.V2
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
