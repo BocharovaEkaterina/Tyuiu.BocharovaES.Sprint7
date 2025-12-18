1239024715aa3c9db69156230fdfa8f30fd48cdb
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-12-18 23:39:04 +0500

    4

---
+++ Tyuiu.BocharovaES.Sprint7.Project.V2/FormChart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tyuiu.BocharovaES.Sprint7.Project.V2
+{
+    public partial class FormChart : Form
+    {
+        public FormChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
